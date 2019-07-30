3906e08f04553a72857e201c7f4a7c3399c7442a
github username <github email address>
2019-07-30 13:01:49 +0100

    added the menu and basic HTML

--- Space-Ship-Game-Front-End/src/index.js
+++ Space-Ship-Game-Front-End/src/index.js
@@ -20,3 +20,69 @@ fetch("http://localhost:3000/users", {
 
 
 })
+// _____________________________________________________________
+const ship = document.querySelector('#ship')
+const background = document.querySelector('#background')
+const form = document.querySelector('#form')
+const musicBtn = document.querySelector('#musicbtn')
+const instBtn = document.querySelector('#instructionsbtn')
+const highScoresBtn = document.querySelector('#highscoresbtn')
+const info = document.querySelector('#info')
+let playToggle = 0
+
+document.addEventListener('DOMContentLoaded', () => {
+    sound()
+    console.log("Loaded")
+})
+
+// MENU FUNCTIONALITY
+function sound(){
+        const music = document.createElement("audio");
+        music.src = "Sparkwood+&+21 copy.mp3";
+        music.id = "music"
+        music.setAttribute("preload", "auto");
+        music.setAttribute("controls", "none");
+        music.style.display = "none";
+        document.body.appendChild(music);
+}
+
+musicBtn.addEventListener("click", () => {
+    const tunes = document.querySelector('#music')
+    if(playToggle == 0){
+        playToggle += 1
+        tunes.play()
+    } else {
+        playToggle -= 1
+        tunes.pause()
+    }
+})
+
+instBtn.addEventListener("click", () => {
+    info.innerHTML = ""
+    const instructions = document.createElement('div')
+    instructions.id = "instructions"
+    instructions.innerHTML = "<p>INSTRUCTIONS:<br><br>Use the arrow keys (&larr;&rarr;&uarr;&darr;) to fly your ship through the asteroid field.<br><br>Use [SPACE] to shoot asteroids and bonus aliens!</p>"
+    info.appendChild(instructions)
+})
+
+highScoresBtn.addEventListener("click", () => {
+    info.innerHTML = ""
+    // INSERT HIGHSCORES HERE USING A FETCH AND RENDER SCORES
+})
+
+
+
+
+
+
+// STARTING THE GAME
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    // fetch(UsersAPI) POST
+    // START GAME
+})
+
+
+
+
+
